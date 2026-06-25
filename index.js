b0cd4826d597a17f9102956cb25009c8e96d933f
Raven Bratcher <ravenbratcher@gmail.com>
2026-06-25 11:22:34 -0500

    added changes from prev assignment

--- index.js
+++ index.js
@@ -1,131 +1,256 @@
+// === class declarations
+/**
+ * @typedef Party
+ * @property {number} id
+ * @property {string} name
+ * @property {string} description
+ * @property {Date} date
+ * @property {string} location
+ * @property {boolean} selected
+ */
+
+class Party {
+  constructor(id, name, description, date, location) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.date = date;
+    this.location = location;
+    this.selected = false;
+  }
+}
+
+/**
+ * @typedef RSVP
+ * @property {number} id
+ * @property {number} guestId
+ * @property {number} eventID
+ */
+
+class RSVP {
+  constructor(id, guestId, eventID) {
+    this.id = id;
+    this.guestId = guestId;
+    this.eventID = eventID;
+  }
+}
+
+/**
+ * @typedef Guest
+ * @property {number} id
+ * @property {string} name
+ * @property {string} email
+ * @property {string} phone
+ * @property {string} bio
+ * @property {string} job
+ */
+
+class Guest {
+  constructor(id, name, email, phone, bio, job) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.bio = bio;
+    this.job = job;
+  }
+}
+
 // === Constants ===
 const BASE = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
-const COHORT = ""; // Make sure to change this!
-const API = BASE + COHORT;
+const COHORT = "/2605-ftb-et-ft";
+
+const PARTY_RESOURCE = "/events";
+const PARTY_API = BASE + COHORT + PARTY_RESOURCE;
+
+const RSVP_RESOURCE = "/rsvps";
+const RSVP_API = BASE + COHORT + RSVP_RESOURCE;
+
+const GUEST_RESOURCE = "/guests";
+const GUEST_API = BASE + COHORT + GUEST_RESOURCE;
 
 // === State ===
+/** @type {Party[]} */
 let parties = [];
+/** @type {Party} */
 let selectedParty;
-let rsvps = [];
-let guests = [];
+/** @type {Map<number, Guest[]>} */
+let guests = new Map();
 
 /** Updates state with all parties from the API */
 async function getParties() {
   try {
-    const response = await fetch(API + "/events");
-    const result = await response.json();
-    parties = result.data;
-    render();
-  } catch (e) {
-    console.error(e);
+    const response = await fetch(PARTY_API);
+    const partiesData = await response.json();
+    for (const party of partiesData.data) {
+      parties.push(
+        new Party(
+          party.id,
+          party.name,
+          party.description,
+          new Date(party.date),
+          party.location,
+        ),
+      );
+    }
+  } catch (error) {
+    console.log("Failed to fetch parties:", error);
   }
 }
 
-/** Updates state with a single party from the API */
-async function getParty(id) {
+/**
+ * Updates state with a single party from the API
+ * @param {number} id
+ */
+async function getParty(partyId) {
   try {
-    const response = await fetch(API + "/events/" + id);
-    const result = await response.json();
-    selectedParty = result.data;
-    render();
-  } catch (e) {
-    console.error(e);
+    const response = await fetch(`${PARTY_API}/${partyId}`);
+    const partyData = await response.json();
+    const newParty = partyData.data;
+    selectedParty = new Party(
+      newParty.id,
+      newParty.name,
+      newParty.description,
+      new Date(newParty.date),
+      newParty.location,
+    );
+  } catch (error) {
+    console.log("Failed to fetch party:", error);
   }
 }
 
-/** Updates state with all RSVPs from the API */
-async function getRsvps() {
+async function getRSVPs(eventId) {
   try {
-    const response = await fetch(API + "/rsvps");
-    const result = await response.json();
-    rsvps = result.data;
-    render();
-  } catch (e) {
-    console.error(e);
+    const rsvps = [];
+
+    const response = await fetch(RSVP_API);
+    const rsvpsData = await response.json();
+    for (const rsvp of rsvpsData.data) {
+      const newRSVP = new RSVP(rsvp.id, rsvp.guestId, rsvp.eventId);
+      if (newRSVP.eventID === eventId) rsvps.push(newRSVP);
+    }
+    //console.log(rsvps);
+
+    return rsvps;
+  } catch (error) {
+    console.log("Failed to fetch RSVPs:", error);
+    return null;
   }
 }
 
-/** Updates state with all guests from the API */
-async function getGuests() {
+async function getGuests(eventId) {
   try {
-    const response = await fetch(API + "/guests");
-    const result = await response.json();
-    guests = result.data;
-    render();
-  } catch (e) {
-    console.error(e);
+    const rsvps = await getRSVPs(eventId);
+    const guestsArr = [];
+    for (const rsvp of rsvps) {
+      const newGuest = await getGuest(rsvp.guestId);
+      guestsArr.push(newGuest);
+    }
+    guests.set(eventId, guestsArr);
+  } catch (error) {
+    console.log("Failed to fetch guests:", error);
+  }
+}
+
+async function getGuest(guestId) {
+  try {
+    const response = await fetch(`${GUEST_API}/${guestId}`);
+    const guestData = await response.json();
+    const newGuestData = guestData.data;
+    const newGuest = new Guest(
+      guestData.data.id,
+      guestData.data.name,
+      guestData.data.email,
+      guestData.data.phone,
+      guestData.data.bio,
+      guestData.data.job,
+    );
+    return newGuest;
+  } catch (error) {
+    console.log("failed to fetch guest:", error);
   }
 }
 
 // === Components ===
 
-/** Party name that shows more details about the party when clicked */
+/**
+ * Party name that shows more details about the party when clicked
+ * @param {Party} party
+ */
 function PartyListItem(party) {
   const $li = document.createElement("li");
-
-  if (party.id === selectedParty?.id) {
-    $li.classList.add("selected");
-  }
-
+  const $a = $li.children;
   $li.innerHTML = `
     <a href="#selected">${party.name}</a>
   `;
-  $li.addEventListener("click", () => getParty(party.id));
+
+  $li.addEventListener("click", async (event) => {
+    for (let i = 0; i < parties.length; i++) {
+      parties[i].selected = false;
+    }
+    party.selected = true;
+    await getParty(party.id);
+    if (!guests.has(party.id)) {
+      await getGuests(party.id);
+    }
+    render();
+  });
+
+  if (party.selected) $li.classList.add("selectedParty");
+
   return $li;
 }
 
 /** A list of names of all parties */
 function PartyList() {
   const $ul = document.createElement("ul");
-  $ul.classList.add("parties");
-
-  const $parties = parties.map(PartyListItem);
-  $ul.replaceChildren(...$parties);
-
+  $ul.classList.add("partyList");
+  const liArr = parties.map(PartyListItem);
+  $ul.replaceChildren(...liArr);
   return $ul;
 }
 
 /** Detailed information about the selected party */
-function SelectedParty() {
+function PartyDetails() {
   if (!selectedParty) {
     const $p = document.createElement("p");
     $p.textContent = "Please select a party to learn more.";
     return $p;
   }
 
-  const $party = document.createElement("section");
-  $party.innerHTML = `
+  const $section = document.createElement("section");
+  $section.classList.add("party");
+  $section.innerHTML = `
     <h3>${selectedParty.name} #${selectedParty.id}</h3>
-    <time datetime="${selectedParty.date}">
-      ${selectedParty.date.slice(0, 10)}
-    </time>
-    <address>${selectedParty.location}</address>
+    <div>
+      <p>${selectedParty.date.toLocaleString()}</p>
+      <p>${selectedParty.location}</p>
+    </div>
     <p>${selectedParty.description}</p>
-    <GuestList></GuestList>
   `;
-  $party.querySelector("GuestList").replaceWith(GuestList());
-
-  return $party;
+  return $section;
 }
 
-/** List of guests attending the selected party */
 function GuestList() {
+  if (!selectedParty) {
+    const $p = document.createElement("p");
+    return $p;
+  }
+
   const $ul = document.createElement("ul");
-  const guestsAtParty = guests.filter((guest) =>
-    rsvps.find(
-      (rsvp) => rsvp.guestId === guest.id && rsvp.eventId === selectedParty.id
-    )
-  );
-
-  // Simple components can also be created anonymously:
-  const $guests = guestsAtParty.map((guest) => {
-    const $guest = document.createElement("li");
-    $guest.textContent = guest.name;
-    return $guest;
-  });
-  $ul.replaceChildren(...$guests);
-
+  $ul.classList.add("guests");
+  const guestList = guests.get(selectedParty.id);
+  const liArr = guestList.map(GuestListItem);
+  $ul.replaceChildren(...liArr);
   return $ul;
+}
+
+function GuestListItem(guest) {
+  const $li = document.createElement("li");
+  $li.innerHTML = `
+    ${guest.name}
+  `;
+  return $li;
 }
 
 // === Render ===
@@ -140,19 +265,18 @@ function render() {
       </section>
       <section id="selected">
         <h2>Party Details</h2>
-        <SelectedParty></SelectedParty>
+        <PartyDetails></PartyDetails>
+        <GuestList></GuestList>
       </section>
     </main>
   `;
-
   $app.querySelector("PartyList").replaceWith(PartyList());
-  $app.querySelector("SelectedParty").replaceWith(SelectedParty());
+  $app.querySelector("PartyDetails").replaceWith(PartyDetails());
+  $app.querySelector("GuestList").replaceWith(GuestList());
 }
 
 async function init() {
   await getParties();
-  await getRsvps();
-  await getGuests();
   render();
 }
 
